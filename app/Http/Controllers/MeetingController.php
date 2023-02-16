0ac12498fa3f7e7ce623bab6b09d26bee5e89c7b
Afeez Aziz <afeezaziz@gmail.com>
2023-02-16 23:44:11 +0800

    123

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -16,12 +16,18 @@ class MeetingController extends Controller
 
     public function show_meeting(Request $request) {
         $id = (int) $request->route('meeting_id');  
-        $meetings = Meeting::find($id);
-        return view('meeting_detail', compact('meetings'));
+        $meeting = Meeting::find($id);
+        return view('meeting_detail', compact('meeting'));
     }
 
     public function update_meeting(Request $request) {
         $id = (int) $request->route('meeting_id');  
-        $meetings = Meeting::find($id);
+        $meeting = Meeting::find($id);
     }
+
+    public function attend_meeting(Request $request) {
+        $id = (int) $request->route('meeting_id');  
+        $meeting = Meeting::find($id);
+        
+    }    
 }
