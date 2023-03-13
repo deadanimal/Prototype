afad9e80c9cb20264bea4250aed9cde0c94ab620
Afeez Aziz <afeezaziz@gmail.com>
2023-03-13 23:44:54 +0800

    321

--- resources/views/layout-auth.blade.php
+++ resources/views/layout-auth.blade.php
@@ -86,7 +86,7 @@
                         </li>
                     @endif
 
-                    @if (Auth::user()->user_type == 'admin' || Auth::user()->->user_type == 'staff')
+                    @if (Auth::user()->user_type == 'admin' || Auth::user()->user_type == 'staff')
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="/projects">
                                 <i class="align-middle me-2 fa fa-fw fa-bank"></i> <span
