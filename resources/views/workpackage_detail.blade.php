ebcb8246566b8196273c08d86a7fe1c49f206178
Afeez Aziz <afeezaziz@gmail.com>
2023-03-15 10:19:20 +0800

    321

--- resources/views/workpackage_detail.blade.php
+++ resources/views/workpackage_detail.blade.php
@@ -21,35 +21,46 @@
                             <h5 class="card-title">Work Package Summary</h5>
                         </div>
 
+                        <table class="table table-striped table-sm">
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    <th>Description</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+                                    <td><b>ID</b></td>
+                                    <td>{{ $wp->id }}</td>
+                                </tr>
+                                <tr>
+                                    <td><b>Name</b></td>
+                                    <td>{{ $wp->name }}</td>
+                                </tr>    
+                                <tr>
+                                    <td><b>Status</b></td>
+                                    <td>{{ $wp->status }}</td>
+                                </tr>      
+                                <tr>
+                                    <td><b>Client</b></td>
+                                    <td>{{ $wp->project->organisation->name }}</td>
+                                </tr>    
+                                <tr>
+                                    <td><b>Project</b></td>
+                                    <td>{{ $wp->project->name }}</td>
+                                </tr>                                                                                                                         
+
+                            </tbody>
+                        </table>                        
+
                         <div class="card-body">
 
-                            <table class="table table-striped table-sm">
-                                <thead>
-                                    <tr>
-                                        <th>Item</th>
-                                        <th>Description</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-    
-                                    <tr>
-                                        <td><b>ID</b></td>
-                                        <td>{{ $wp->id }}</td>
-                                    </tr>
-                                    <tr>
-                                        <td><b>Name</b></td>
-                                        <td>{{ $wp->name }}</td>
-                                    </tr>                                    
-    
-                                </tbody>
-                            </table>
 
                     
                             Estimate Delivery: {{ $wp->estimate_delivery }} <br/>
                             Package Type: {{ $wp->package_type }} <br/>
                             Package level: {{ $wp->package_level }} <br/>
-                            Status: {{ $wp->status }} <br/>
-                            Project: {{ $wp->project->organisation->name }} - {{ $wp->project->name }} <br/>
                             Resource: 
                             @if($wp->resource_id)
                                 {{ $wp->resource->user->name }}
