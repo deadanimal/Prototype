85ab43fa070fb5d51c6e15e239cd07b4cb3f0873
Afeez Aziz <afeezaziz@gmail.com>
2023-03-20 13:24:00 +0800

    321

--- resources/views/emails/workpackage_assigned.blade.php
+++ resources/views/emails/workpackage_assigned.blade.php
@@ -1,3 +1,4 @@
+<h3>Work Package Detail</h3>
 <table class="table table-striped table-sm">
     <tbody>
 
@@ -20,33 +21,39 @@
         <tr>
             <td><b>Project</b></td>
             <td>{{ $wp->project->name }}</td>
-        </tr>                                                                                                                         
+        </tr>      
+        <tr>
+            <td><b>Resource</b></td>
+            <td>{{ $wp->resource->user->name }}</td>
+        </tr>
+        <tr>
+            <td><b>Reviewer</b></td>
+            <td>{{ $wp->reviewer->user->name }}</td>
+        </tr>            
+        <tr>
+            <td><b>Coordinator</b></td>
+            <td>{{ $wp->coordinator->name }} </td>
+        </tr>     
+        <tr>
+            <td><b>Estimate Delivery</b></td>
+            <td>{{ $wp->estimate_delivery }} </td>
+        </tr>     
+        <tr>
+            <td><b>Package Type</b></td>
+            <td>{{ $wp->package_type  }} </td>
+        </tr> 
+        <tr>
+            <td><b>Package Level</b></td>
+            <td>{{ $wp->package_level  }} </td>
+        </tr>                        
+        <tr>
+            <td><b>Created</b></td>
+            <td>{{ $wp->created_at }} </td>
+        </tr>             
 
     </tbody>
 </table>                        
 
 <div class="card-body">
-
-
-
-    Estimate Delivery: {{ $wp->estimate_delivery }} <br/>
-    Package Type: {{ $wp->package_type }} <br/>
-    Package level: {{ $wp->package_level }} <br/>
-    Resource: 
-    @if($wp->resource_id)
-        {{ $wp->resource->user->name }}
-    @else
-        -
-    @endif
-     <br/>
-    Reviewer: 
-    @if($wp->reviewer_id)
-        {{ $wp->reviewer->user->name }}
-    @else
-        -
-    @endif
-    <br/>
-    Coordinator: {{ $wp->coordinator->name }} <br/>
-    Created: {{ $wp->created_at }} <br/>
     Remarks: {{ $wp->remarks }} <br/>
 </div>
--- resources/views/emails/workpackage_reviewed.blade.php
+++ resources/views/emails/workpackage_reviewed.blade.php
@@ -1,3 +1,19 @@
+<h3>Work Package Review</h3>
+<table class="table table-striped table-sm">
+    <tbody>
+
+        <tr>
+            <td><b>Status</b></td>
+            <td>{{ $review->status }}</td>
+        </tr>
+        <tr>
+            <td><b>Remarks</b></td>
+            <td>{{ $review->remarks }}</td>
+        </tr> 
+    </tbody>
+</table>
+
+<h3>Work Package Detail</h3>
 <table class="table table-striped table-sm">
     <tbody>
 
@@ -20,36 +36,39 @@
         <tr>
             <td><b>Project</b></td>
             <td>{{ $wp->project->name }}</td>
-        </tr>                                                                                                                         
+        </tr>      
+        <tr>
+            <td><b>Resource</b></td>
+            <td>{{ $wp->resource->user->name }}</td>
+        </tr>
+        <tr>
+            <td><b>Reviewer</b></td>
+            <td>{{ $wp->reviewer->user->name }}</td>
+        </tr>            
+        <tr>
+            <td><b>Coordinator</b></td>
+            <td>{{ $wp->coordinator->name }} </td>
+        </tr>     
+        <tr>
+            <td><b>Estimate Delivery</b></td>
+            <td>{{ $wp->estimate_delivery }} </td>
+        </tr>     
+        <tr>
+            <td><b>Package Type</b></td>
+            <td>{{ $wp->package_type  }} </td>
+        </tr> 
+        <tr>
+            <td><b>Package Level</b></td>
+            <td>{{ $wp->package_level  }} </td>
+        </tr>                        
+        <tr>
+            <td><b>Created</b></td>
+            <td>{{ $wp->created_at }} </td>
+        </tr>             
 
     </tbody>
 </table>                        
 
 <div class="card-body">
-
-
-
-    Estimate Delivery: {{ $wp->estimate_delivery }} <br/>
-    Package Type: {{ $wp->package_type }} <br/>
-    Package level: {{ $wp->package_level }} <br/>
-    Resource: 
-    @if($wp->resource_id)
-        {{ $wp->resource->user->name }}
-    @else
-        -
-    @endif
-     <br/>
-    Reviewer: 
-    @if($wp->reviewer_id)
-        {{ $wp->reviewer->user->name }}
-    @else
-        -
-    @endif
-    <br/>
-    Coordinator: {{ $wp->coordinator->name }} <br/>
-    Created: {{ $wp->created_at }} <br/>
     Remarks: {{ $wp->remarks }} <br/>
 </div>
-
-Review status: {{ $review->status }} <br/>
-Review Remarks: {{ $review->remarks }} <br/>
