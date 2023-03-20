1bacef61662c9e36ad07899c8104f74e760ba046
Afeez Aziz <afeezaziz@gmail.com>
2023-03-20 17:10:33 +0800

    nn

--- resources/views/project_detail.blade.php
+++ resources/views/project_detail.blade.php
@@ -649,7 +649,11 @@
                                         </div>                            
             
                                         <button type="submit" class="btn btn-primary">Create Ticket</button>
-                                    </form>                                      
+                                    </form>     
+                                    
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#defaultModalPrimary">
+										Primary
+									</button>
 
                                 </div>
                             </div>
@@ -664,7 +668,24 @@
 
 
 
-
+            <div class="modal fade" id="defaultModalPrimary" tabindex="-1" style="display: none;" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Default modal</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body m-3">
+                            <p class="mb-0">Use Bootstrap’s JavaScript modal plugin to add dialogs to your site for lightboxes, user
+                                notifications, or completely custom content.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
 
