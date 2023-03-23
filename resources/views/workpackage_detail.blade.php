d6f96dd54452511308e8b4494fdb4db4818a6ff1
Afeez Aziz <afeezaziz@gmail.com>
2023-03-23 19:07:25 +0800

    321

--- resources/views/workpackage_detail.blade.php
+++ resources/views/workpackage_detail.blade.php
@@ -189,6 +189,7 @@
 
                             @if (Auth::user()->resource->id == $wp->resource_id ||
                                     Auth::user()->resource->id == $wp->reviewer_id ||
+                                    Auth::user()->resource->resource_type == 'all' ||
                                     Auth::user()->resource->resource_type == 'pmo')
                                 <form action="/workpackages/{{ $wp->id }}/review" method="POST"
                                     enctype="multipart/form-data">
