58eed593c2eafee429a195fd2ca9ecd144359a9b
Afeez Aziz <afeezaziz@gmail.com>
2023-03-26 15:59:02 +0800

    321

--- app/Models/Kitab.php
+++ app/Models/Kitab.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\LogOptions;
 
 class Kitab extends Model
 {
